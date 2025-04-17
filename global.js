faf26c4b6666948b5314118d48b2df9b48809b7b
JasSandhu14 <jsandhu@ucsd.edu>
2025-04-16 22:44:44 -0700

    include log statements for debugging

--- global.js
+++ global.js
@@ -5,22 +5,18 @@ function $$(selector, context = document) {
 }
 
 // let navLinks = $$("nav a");
-// console.log(navLinks);
 
 // let currentLink = navLinks.find(
 //     (a) => a.host === location.host && a.pathname === location.pathname,
 // );
 
-// console.log(currentLink);
 // currentLink?.classList.add('current');
-// console.log(currentLink);
 
 let pages = [
     { url: '', title: 'Home' },
     { url: 'resume/', title: 'Resume'},
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact'},
-    // add the rest of your pages here
 ];
 
 let nav = document.createElement('nav');
@@ -31,11 +27,14 @@ for (let p of pages) {
     let title = p.title;
     // next step: create link and add it to nav
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-}  
 
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/"                           // Local server
     : "/jassandhu14.github.io/";    // GitHub Pages repo name
 
+    console.log(url)
 
-url = !url.startsWith('http') ? BASE_PATH + url : url;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+    console.log(url)
+}  
