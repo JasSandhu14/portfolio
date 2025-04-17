2aee76b19d3fcc89b7c27252c2c87df7aa886d2c
Jas Sandhu <jsandhu@ucsd.edu>
2025-04-17 14:14:04 -0700

    added dark mode and fixed links

--- global.js
+++ global.js
@@ -16,25 +16,60 @@ let pages = [
     { url: '', title: 'Home' },
     { url: 'resume/', title: 'Resume'},
     { url: 'projects/', title: 'Projects' },
-    { url: 'contact/', title: 'Contact'},
+    { url: 'https://github.com/JasSandhu14', title: 'GitHub'},
+    { url: 'contact/', title: 'Contact'}
 ];
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    // next step: create link and add it to nav
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  let url = p.url;
+  let title = p.title;
+  // next step: create link and add it to nav
 
-    const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-    ? "/"                           // Local server
-    : "/jassandhu14.github.io/";    // GitHub Pages repo name
+  const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                                    // Local server
+  : "/jassandhu14.github.io/portfolio/";    // GitHub Pages repo name
 
-    console.log(url)
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
 
-    url = !url.startsWith('http') ? BASE_PATH + url : url;
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
 
-    console.log(url)
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+  
+  if (a.host != location.host){
+    a.target = "_blank"
+  }
 }  
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+			<option value='light dark'>Automatic</option>
+      <option value='light'>Light</option>
+      <option value='dark'>Dark</option>
+		</select>
+	</label>`,
+);
+
+const select = document.querySelector('select')
+
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+
+if ("colorScheme" in localStorage){
+  select.value = localStorage.colorScheme;
+  document.documentElement.style.setProperty('color-scheme', select.value);
+}
