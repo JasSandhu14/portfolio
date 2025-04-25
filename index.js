ca26a455abf16e485f539f263432efd73175097e
JasSandhu14 <jsandhu@ucsd.edu>
2025-04-25 15:29:33 -0700

    formatting

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
-console.log(latestProjects)
 renderProjects(latestProjects, projectsContainer, 'h2');
 
 const githubData = await fetchGitHubData('jassandhu14');
