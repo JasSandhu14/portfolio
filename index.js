dea9beb9d239ab844e58c9ef8d3ebfefcdf4df7b
JasSandhu14 <jsandhu@ucsd.edu>
2025-04-24 17:09:19 -0700

    add github stats to homepage

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { fetchJSON, renderProjects} from './global.js';
-/* removed fetchGithubData from imports as it was not showing projects correctly */
+import { fetchJSON, renderProjects, fetchGitHubData} from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
@@ -7,3 +6,19 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 console.log(latestProjects)
 renderProjects(latestProjects, projectsContainer, 'h2');
+
+const githubData = await fetchGitHubData('jassandhu14');
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+    profileStats.innerHTML = `
+        <h2>GitHub Stats</h2>
+          <dl>
+            <dt>Followers</dt><dd>${githubData.followers}</dd>
+            <dt>Following</dt><dd>${githubData.following}</dd>
+            <dt>Public Repos</dt><dd>${githubData.public_repos}</dd>
+            <dt>Public Gists</dt><dd>${githubData.public_gists}</dd>
+          </dl>
+      `;
+  }
+  
